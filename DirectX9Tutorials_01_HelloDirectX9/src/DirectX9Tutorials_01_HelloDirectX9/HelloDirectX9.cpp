7d9910ecb4c6fe2c065a53e8d7827b446aef261f
hzzcle <hzzcle@163.com>
2015-11-25 08:41:17 +0800

    添加注释。

--- DirectX9Tutorials_01_HelloDirectX9/src/DirectX9Tutorials_01_HelloDirectX9/HelloDirectX9.cpp
+++ DirectX9Tutorials_01_HelloDirectX9/src/DirectX9Tutorials_01_HelloDirectX9/HelloDirectX9.cpp
@@ -1,3 +1,9 @@
+//***************************************************************************
+// main.cpp by hzzcle (C) 2015 All Rights Reserved.
+// 博文地址:http://blog.csdn.net/hzzcle/article/details/49717695
+// github地址:https://github.com/hzzcle/
+//***************************************************************************
+
 #include <windows.h>        // windows基础头文件
 #include <windowsx.h>       // 实用宏定义头文件
 #include <tchar.h>          // windows通用字符串处理头文件
